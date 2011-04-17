208efa0558c2fa70e87722d78ef95e23fc561a88
Derek Hoy <derek@snowcloud.co.uk>
2011-04-17 17:01:09 +0100

    Added check for MEDIA_URL and STATIC_URL in path to allow use with dev server.

--- sitedown/middleware.py
+++ sitedown/middleware.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+import urlparse
+
 class SitedownMiddleware(object):
     """
     Site down middleware. If enabled, each Django-powered URL will
@@ -14,10 +16,12 @@ class SitedownMiddleware(object):
         self.template = getattr(settings, 'SITEDOWN_TEMPLATE', 'sitedown/default.html' )
     
     def process_request(self, request):
-        if getattr(settings, 'SITEDOWN_DISABLE', False):
+        if  getattr(settings, 'SITEDOWN_DISABLE', False) or \
+            request.path.startswith('/admin') or \
+            request.path.startswith(urlparse.urlparse(settings.MEDIA_URL).path) or \
+            (getattr(settings, 'STATIC_URL', False) and request.path.startswith(urlparse.urlparse(settings.STATIC_URL))):
             return None
-        if request.path.startswith('/admin'):
-            return None
+        # print settings.STATIC_PATH
         return render_to_response(self.template,
             RequestContext( request, {}))
         
